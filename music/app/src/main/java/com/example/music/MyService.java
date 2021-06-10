411caab5f4370fcf54f0f6b665f06fe778b5ab89
saikumar2311 <72977244+saikumar2311@users.noreply.github.com>
2021-06-10 17:58:23 +0530

    10/06/2021

---
+++ music/app/src/main/java/com/example/music/MyService.java
@@ -0,0 +1,40 @@
+package com.example.music;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.view.View;
+
+public class MyService extends Service {
+    MediaPlayer mp;
+
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mp= MediaPlayer.create(getApplicationContext(),R.raw.song);
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        mp.start();
+        return super.onStartCommand(intent,flags,startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mp.stop();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
